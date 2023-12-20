6af01cf6fe76406732b7d80d646771c867ab76f6
Fabio <fabio@Fabios-MacBook-Pro.local>
2023-12-20 10:53:33 -0300

    organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './reset.scss'
+import './styles/reset.scss'
 import './styles/variables.scss'
 
 const inter = Inter({ subsets: ['latin'] })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import './page.scss'
+import './styles/page.scss'
 
 export default function Home() {
   return (
